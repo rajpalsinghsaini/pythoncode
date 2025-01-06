53488f2cec240b613b9d72106130b11fae0c9ce7
rajpalsinghsaini <enggrajpal@gmail.com>
2025-01-06 19:14:19 +0530

    Create scripted.groovy

---
+++ scripted.groovy
@@ -0,0 +1,11 @@
+node {
+    stage('Build') {
+        echo "Build success in scripted pipeline"
+    }
+    stage('Test') {
+        echo "Test success in scripted pipeline"
+    }
+    stage('Deploy') {
+        echo "Deployment success in scripted pipeline"
+    }
+}
