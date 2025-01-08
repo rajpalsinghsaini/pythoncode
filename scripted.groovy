2fae634c471fdfb50cd5cd27515554c76078e4d9
rajpalsinghsaini <enggrajpal@gmail.com>
2025-01-08 10:44:45 +0530

    scripted pipeline file edited in vscode

--- scripted.groovy
+++ scripted.groovy
@@ -1,9 +1,9 @@
 node {
     stage('Build') {
-        echo "Build success in scripted pipeline"
+        echo "Build success in scripted pipeline in VScode"
     }
     stage('Test') {
-        echo "Test success in scripted pipeline"
+        echo "Test success in scripted pipeline in VSCode"
     }
     stage('Deploy') {
         echo "Deployment success in scripted pipeline Updated"
