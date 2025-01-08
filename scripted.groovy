56592c0808a97f1f268bcc5532e410ff3764e6e6
rajpalsinghsaini <enggrajpal@gmail.com>
2025-01-08 10:48:20 +0530

    modified

--- scripted.groovy
+++ scripted.groovy
@@ -1,11 +1,11 @@
 node {
     stage('Build') {
-        echo "Build success in scripted pipeline in VScode Updated"
+        echo "Build success in scripted pipeline in VScode Updated again" 
     }
     stage('Test') {
-        echo "Test success in scripted pipeline in VSCode Updated"
+        echo "Test success in scripted pipeline in VSCode Updated again"
     }
     stage('Deploy') {
-        echo "Deployment success in scripted pipeline Updated again"
+        echo "Deployment success in scripted pipeline Updated again 1234"
     }
 }
